95eb872048f3e466d55e23e370c3634afe9f1b4f
Michel Deiman <michel_deiman@imac-van-michel.local>
2017-03-26 06:44:26 +0200

    Initial Commit

---
+++ Smashtag/Tweet.swift
@@ -0,0 +1,14 @@
+//
+//  Tweet.swift
+//  Smashtag
+//
+//  Created by Michel Deiman on 24/03/2017.
+//  Copyright © 2017 Michel Deiman. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Tweet: NSManagedObject {
+
+}
---
+++ Smashtag/TwitterUser.swift
@@ -0,0 +1,15 @@
+//
+//  TwitterUser.swift
+//  Smashtag
+//
+//  Created by Michel Deiman on 24/03/2017.
+//  Copyright © 2017 Michel Deiman. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+class TwitterUser: NSManagedObject {
+
+}
